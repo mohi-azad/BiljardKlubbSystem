260a1a57c523732f387f1f4034802a107dcc2f64
Mohsen <Mohsen@DESKTOP-8AT1P8G>
2025-11-16 14:28:40 +0100

    Add project files.

---
+++ Models/Class.cs
@@ -0,0 +1,23 @@
+﻿public class Member
+{
+    public int MemberID { get; set; }
+    public String FirstName { get; set; }
+    public String LastName { get; set; }
+    public int Age { get; set; }
+    public int Score { get; set; }
+    public Member()
+    {
+        FirstName = "";
+        LastName = "";
+        Age = 0;
+        Score = 0;
+    }
+    public Member(int memberID, String firstName, String lastName, int age, int score)
+    {
+        MemberID = memberID;
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Score = score;
+    }
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,20 @@
+﻿public class Tournament
+{
+    public int TournamentID { get; set; }
+    public string Type { get; set; }
+    public DateTime Date { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public Tournament()
+    {
+        Type = "";
+        Date = DateTime.Today;
+        StartTime = new TimeSpan();
+    }
+    public Tournament(int tournamentID, string type,DateTime date,TimeSpan startTime)
+    {
+        TournamentID = tournamentID;
+        Type = type;
+        Date = date;
+        StartTime = startTime;
+    }
+}
